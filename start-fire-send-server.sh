f108116f6d6d295feba9251fd3b72b8a9ea3c4d0
nebulaorion <musa.arslan@childrens.harvard.edu>
2023-10-10 15:21:38 -0400

    Modified the start-fire-send-server.sh appropriately so its much shorter.

--- start-fire-send-server.sh
+++ start-fire-send-server.sh
@@ -8,4 +8,21 @@
 #           for example 0.1 stands for 100 miliseconds delay between each image file
 #           for example for a dataset with 56 images per volume with 1 seconds per volume (56 images per second), d should be 1/56
 
-sudo docker run --rm -it -v ./save:/opt/code/python-fire-server-base/save -v ./logs:/opt/code/python-fire-server-base/logs python-fire-server:send -v -H "172.17.0.1" -p 9002 -l ./logs/python-ismrmrd-server.log -f ./save/measurement-20231010T155525.hdf5 -d 0.017
+# usage
+# sh start-fire-send-server.sh measurement-20231010T155525.hdf5 0.01
+# first input is the h5 file name
+# second input is the delay in seconds
+
+work_dir=/opt/code/python-fire-server-base
+
+if [ $# -lt 2 ];
+then
+  echo "$0: Missing arguments -f file name -d delay (s)"
+  exit 1
+elif [ $# -gt 2 ];
+then
+  echo "$0: Too many arguments: $@"
+  exit 1
+else
+  sudo docker run --rm -it -v ./save:${work_dir}/save -v ./logs:${work_dir}/logs python-fire-server:send -v -H "172.17.0.1" -p 9002 -l ${work_dir}/logs/python-ismrmrd-server.log -f ${work_dir}/save/$1 -d $2
+fi
